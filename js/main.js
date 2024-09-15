36fdc8d9b5f1051028ff84c144876fe576018bb7
Amirjon707 <amirjonxamdamov2009@gmail.com>
2024-09-15 13:30:15 +0500

    header

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 const BASE_URL = "http://localhost:5050";
 
-const con = document.querySelector(".container");
+// const con = document.querySelector(".container");
 
-fetch(`${BASE_URL}/goods`)
-    .then((response) => response.json())
-    .then((data) => get(data));
+// fetch(`${BASE_URL}/goods`)
+//     .then((response) => response.json())
+//     .then((data) => get(data));
 
 
-function get(e) {
-    e.forEach((item) => {
-        const box = document.createElement("div");
-        box.classList.add("card");
-        box.innerHTML = `
-        <img src="${item.media[0]}">
-    <h2>${item.title}</h2>
-    <p>${item.description}</p>
-    <p>${item.price}</p>
-    `;
-        con.appendChild(box);
-    });
-}
+// function get(e) {
+//     e.forEach((item) => {
+//         const box = document.createElement("div");
+//         box.classList.add("card");
+//         box.innerHTML = `
+//         <img src="${item.media[0]}">
+//     <h2>${item.title}</h2>
+//     <p>${item.description}</p>
+//     <p>${item.price}</p>
+//     `;
+//         con.appendChild(box);
+//     });
+// }
