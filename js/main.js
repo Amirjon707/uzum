aa55d459c8c08939e8bce2e9bad1b55e96c43b6a
Amirjon707 <amirjonxamdamov2009@gmail.com>
2024-09-15 11:26:26 +0500

    xz

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-let BASE_URL = "http://localhost:5050";
+const BASE_URL = "http://localhost:5050";
+
+const con = document.querySelector(".container");
 
 fetch(`${BASE_URL}/goods`)
     .then((response) => response.json())
-    .then((data) => console.log(data.products));
+    .then((data) => get(data));
+
+function get(e) {
+    e.forEach((item) => {
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.innerHTML = `
+    <h2>${item.title}</h2>
+    <p>${item.description}</p>
+    <p>${item.price}</p>
+    `;
+        con.appendChild(div);
+    });
+}
