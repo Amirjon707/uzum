2de748abf4e4742141d6927d867a9b31449f280c
Amirjon707 <amirjonxamdamov2009@gmail.com>
2024-10-03 15:05:00 +0500

    fix bags

--- wite-project/js/main.js
+++ wite-project/js/main.js
@@ -2,19 +2,36 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5050";
 
+const furniture = document.querySelector('#furniture')
+const audio = document.querySelector('#audio')
+const pc = document.querySelector('#pc')
+const tv = document.querySelector('#tv')
+
+function runFunc(data) {
+    createCard(data, furniture);
+    // createCard(data, audio);
+    // createCard(data, tv);
+    // createCard(data, pc);
+}
+
+
 const swiper = async () => {
     try {
         const res = await axios.get(`${BASE_URL}/goods`);
 
         createSwiper(res.data);
+        runFunc(res.data)
     } catch (error) {
         console.error(error);
     }
 };
 
+
+
+
 function createSwiper(data) {
     const swiperContainer = document.querySelector("swiper-container");
-    for (let i = 0; swiperContainer.children.length <= 20; i++) {
+    for (let i = 0; swiperContainer.children.length <= 30; i++) {
         const swiperElem = document.createElement("swiper-slide");
 
         const swiper = document.createElement("div");
@@ -68,10 +85,58 @@ catalogBtn.addEventListener("click", () => {
         catalogIcon[0].classList.add("hide");
     }
 });
-let price1 = `2000000`;
-for (let i = 0; i < price1.length; i++) {
-    if (i % 3 == 0) {
-        let price2 = price1.split("").join(',');
-        console.log(price2[i]);
+
+const createCard = (data, con) => {
+    for (let i = 0; con.children.length < 6; i++) {
+        let row = con.querySelector('.row')
+        
+
+        
+        
+        if (con.id.toLowerCase() == data[i].type.toLowerCase()) {
+            
+            const cart = document.createElement("div");
+            cart.classList.add("cart", "col-lg-3", "col-xl-3", "col-md-1", "col-5");
+            cart.id = data[i].id;
+            cart.innerHTML = `
+            <div class="cart-img">
+            <img src="${data[i].media[0]}" alt="${data[i].name}">
+            </div>
+            <div class="cart-info">
+            <h3 class="title">${data[i].title}</h3>
+            <p class="rating">
+                <ion-icon name="star" class="ratingIcon"></ion-icon>
+                <span>${data[i].rating}</span>
+            </p>
+            <p>${format((data[i].price + ''))} <sup>сум</sup></p>
+            </div>
+            `;
+            row.append(cart);
+        }
     }
+};
+
+
+
+
+
+
+
+
+// function numberWithCommas(x) {
+//     return x.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+// }
+
+
+
+function format(str) {
+    const s = str.length;
+    const chars = str.split('');
+    const strWithSpaces = chars.reduceRight((acc, char, i) => {
+        const spaceOrNothing = ((((s - i) % 3) === 0) ? ' ' : '');
+        return (spaceOrNothing + char + acc);
+    }, '');
+    console.log(((strWithSpaces[0] === ' ') ? strWithSpaces.slice(1) : strWithSpaces));
+    
+    return ((strWithSpaces[0] === ' ') ? strWithSpaces.slice(1) : strWithSpaces);
 }
