551196022c7c711d0254c05c9849868bc35afa96
Amirjon707 <amirjonxamdamov2009@gmail.com>
2024-10-03 16:43:09 +0500

    cart

--- wite-project/js/main.js
+++ wite-project/js/main.js
@@ -2,32 +2,28 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5050";
 
-const furniture = document.querySelector('#furniture')
-const audio = document.querySelector('#audio')
-const pc = document.querySelector('#pc')
-const tv = document.querySelector('#tv')
+const furniture = document.querySelector("#furniture");
+const audio = document.querySelector("#audio");
+const pc = document.querySelector("#pc");
+const tv = document.querySelector("#tv");
+const kitchen = document.querySelector("#kitchen");
 
-function runFunc(data) {
-    createCard(data, furniture);
-    // createCard(data, audio);
-    // createCard(data, tv);
-    // createCard(data, pc);
-}
-
+let conArr = [furniture, audio, tv, pc, kitchen];
 
 const swiper = async () => {
     try {
         const res = await axios.get(`${BASE_URL}/goods`);
 
         createSwiper(res.data);
-        runFunc(res.data)
+
+        for (let i = 0; i < conArr.length; i++) {
+            createCard(res.data, conArr[i]);
+            // createCard(res.data, pc);
+        }
     } catch (error) {
         console.error(error);
     }
 };
-
-
-
 
 function createSwiper(data) {
     const swiperContainer = document.querySelector("swiper-container");
@@ -50,7 +46,7 @@ function createSwiper(data) {
 
         const price = document.createElement("p");
         price.classList.add("price");
-        price.innerHTML = `${data[i].price} <sup>сум</sup>`;
+        price.innerHTML = `${format(data[i].price + "")} <sup>сум</sup>`;
 
         const imgBox = document.createElement("div");
         imgBox.classList.add("img");
@@ -87,16 +83,25 @@ catalogBtn.addEventListener("click", () => {
 });
 
 const createCard = (data, con) => {
-    for (let i = 0; con.children.length < 6; i++) {
-        let row = con.querySelector('.row')
+    const findChildren = con.querySelector(".row");
+    let nasiya = 0;
+
+
+    for (let i = 0; findChildren.children.length < 6; i++) {
+        let row = con.querySelector(".row");
+        nasiya = Math.floor((data[i].price + (data[i].price / 100) * 5) / 12)
         
 
-        
-        
+
         if (con.id.toLowerCase() == data[i].type.toLowerCase()) {
-            
             const cart = document.createElement("div");
-            cart.classList.add("cart", "col-lg-3", "col-xl-3", "col-md-1", "col-5");
+            cart.classList.add(
+                "cart",
+                "col-lg-3",
+                "col-xl-3",
+                "col-md-1",
+                "col-5"
+            );
             cart.id = data[i].id;
             cart.innerHTML = `
             <div class="cart-img">
@@ -106,9 +111,11 @@ const createCard = (data, con) => {
             <h3 class="title">${data[i].title}</h3>
             <p class="rating">
                 <ion-icon name="star" class="ratingIcon"></ion-icon>
-                <span>${data[i].rating}</span>
+                &nbsp;&nbsp;
+                <span style="letter-spacing: 1.5px;">${data[i].rating}</span>
             </p>
-            <p>${format((data[i].price + ''))} <sup>сум</sup></p>
+            <p class="nasiya1"><span class="nasiya">${format(nasiya + '')} сум/мес</span></p>
+            <p class="price">${format(data[i].price + "")} <sup>сум</sup></p>
             </div>
             `;
             row.append(cart);
@@ -116,27 +123,13 @@ const createCard = (data, con) => {
     }
 };
 
-
-
-
-
-
-
-
-// function numberWithCommas(x) {
-//     return x.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-// }
-
-
-
 function format(str) {
     const s = str.length;
-    const chars = str.split('');
+    const chars = str.split("");
     const strWithSpaces = chars.reduceRight((acc, char, i) => {
-        const spaceOrNothing = ((((s - i) % 3) === 0) ? ' ' : '');
-        return (spaceOrNothing + char + acc);
-    }, '');
-    console.log(((strWithSpaces[0] === ' ') ? strWithSpaces.slice(1) : strWithSpaces));
-    
-    return ((strWithSpaces[0] === ' ') ? strWithSpaces.slice(1) : strWithSpaces);
+        const spaceOrNothing = (s - i) % 3 === 0 ? " " : "";
+        return spaceOrNothing + char + acc;
+    }, "");
+
+    return strWithSpaces[0] === " " ? strWithSpaces.slice(1) : strWithSpaces;
 }
